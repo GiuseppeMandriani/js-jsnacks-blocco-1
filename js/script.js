524471c232281e5e9005d45ac4cf299999e28fd0
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-03-25 15:23:29 +0100

    add snack 4

--- js/script.js
+++ js/script.js
@@ -64,41 +64,56 @@ Generatore di “nomi e cognomi” casuali: prendendo una lista di nomi e una li
 
 // Creo liste di nomi e cognomi
 
-var nomi = ['giuseppe', 'michele', 'sara', 'pinco', 'davide'];
-var cognomi = ['mandriani', 'patruno', 'preziosa', 'vece', 'pallino'];
-var invitati = [];
+// var nomi = ['giuseppe', 'michele', 'sara', 'pinco', 'davide'];
+// var cognomi = ['mandriani', 'patruno', 'preziosa', 'vece', 'pallino'];
+// var invitati = [];
 
-for (var i = 0; i < 3; i++){
-    // Generazione nomi random
-    var nomiRandom = Math.floor(Math.random() * nomi.length);
-    // console.log(nomiRandom);
+// for (var i = 0; i < 3; i++){
+//     // Generazione nomi random
+//     var nomiRandom = Math.floor(Math.random() * nomi.length);
+//     // console.log(nomiRandom);
     
 
-    // Generazione cognomi Cognomi Random
-    var cognomiRandom = Math.floor(Math.random() * cognomi.length);
-    // console.log(cognomiRandom);
+//     // Generazione cognomi Cognomi Random
+//     var cognomiRandom = Math.floor(Math.random() * cognomi.length);
+//     // console.log(cognomiRandom);
 
-    // Genero nuova variabile per combinazione e aggiungo alla lista di invitati
-    var fullName = nomi[nomiRandom] + ' ' + cognomi[cognomiRandom];
+//     // Genero nuova variabile per combinazione e aggiungo alla lista di invitati
+//     var fullName = nomi[nomiRandom] + ' ' + cognomi[cognomiRandom];
 
-    invitati.push(fullName);
+//     invitati.push(fullName);
     
-}
+// }
 
-console.log(invitati); // Stampo lista di 3  nomi e cognomi random
+// console.log(invitati); // Stampo lista di 3  nomi e cognomi random
 
 
-
+/********************************************************************************************** */
 
 
 /*
 Snack 4
 Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari*/
 
+// Creo array di numeri interi 
 
+// var numeri =[1, 5, 9, 10, 99, 15];
+// console.log(numeri);
+// var somma = 0;
+
+// for(var i=0; i<numeri.length; i++){
+//     if(i % 2 !== 0){
+//         somma += numeri[i]; // Aggiungo a somma il valore di i
+//     }
+// }
+
+// console.log('La somma dei numeri in posizione dispari dell\' array è: ', somma);  // Stampo la somma
+
+/********************************************************************************************************* */
 
 
 /*
 Snack 5 - Bonus
 Crea due array di numeri che hanno un numero di elementi diversi.
 Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.*/
+
